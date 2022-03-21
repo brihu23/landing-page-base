04bcf71b7d891154661ac4bcae8d26206c2992b8
brihu.sundararaman@yale.edu <brihu.sundararaman@yale.edu>
2022-03-21 19:13:42 -0400

    case updates

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Header from '../components/home/header.tsx'
-import Features from '../components/home/features.tsx'
-import Benefits from '../components/home/benefits.tsx'
-import Faqs from '../components/home/faqs.tsx'
+import Header from '../components/home/Header.tsx'
+import Features from '../components/home/Features.tsx'
+import Benefits from '../components/home/Benefits.tsx'
+import Faqs from '../components/home/Faqs.tsx'
 import Testimonials from '../components/home/Testimonials.tsx'
 
 export default function Home() {
